30d2565e6114d8788d7858b1e6ba39d17fa720ff
xilanhuadechuntian <Lensgcx@163.com>
2019-03-21 11:28:04 +0800

    MDEditor
    MDEditor

---
+++ src/pages/MDEditor/distributeForm.tsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import { Form, Row, Col, Input, Icon } from 'antd';
+import { connect } from 'dva';
+import { Component } from '@/components/BaseComponent';
+import styles from './distributeForm.less';
+
+interface DistributeFormProps {
+  fileExt: Array<string>;
+  handleSave: Function; //外部传入的提交保存事件
+}
+
+connect(({ markdownEditor }) => ({
+  contentText: markdownEditor.contentText,
+}));
+
+let id = 0;
+
+class DistributeForm extends Component<DistributeFormProps> {
+  state = {
+    fileExt: ['md'], //文件名称- 合法后缀
+  };
+
+  componentDidMount() {
+    console.log('distributeForm - componentDidMount !!!!');
+    // this.props.onRef(this);
+    this.initFormField(1);
+  }
+
+  validateFilename = (rule, value, callback) => {
+    if (!value) {
+      callback('请输入文件名称');
+    } else {
+      const FileExt = value.replace(/.+\./, '');
+      if (!this.state.fileExt.some(e => e === FileExt)) {
+        callback('请输入正确后缀的文件名称');
+      }
+      callback();
+    }
+  };
+
+  /**
+   * init form field
+   * @param count
+   */
+  initFormField(count: number = 1) {
+    let i = 0;
+    while (i < count) {
+      this.add();
+      i++;
+    }
+  }
+
+  /**
+   * add one input field
+   */
+  add = () => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(id++);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  };
+
+  /**
+   *  remove one input field
+   */
+  remove = k => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  };
+
+  /**
+   * form submit
+   * @param e
+   */
+  handleSubmit = e => {
+    this.props.form.validateFields(
+      (err: any, values: object): void => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+          //handle save data - Merge editor data and submit it to background
+          this.props.handleSave(e, values);
+        }
+      }
+    );
+  };
+
+  /**
+   * form data reset
+   */
+  handleReset = () => {
+    this.props.form.resetFields();
+
+    this.initFormField(1); //init form field
+  };
+
+  render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    getFieldDecorator('keys', { initialValue: [] });
+    const keys = getFieldValue('keys');
+
+    const formItems = keys.map((k, index) => (
+      <Col span={5} key={index} style={{ marginRight: index + 1 === keys.length ? '16px' : null }}>
+        <Form.Item label={`路径 ${index + 1}`} key={k} hasFeedback>
+          {getFieldDecorator(`path-${index + 1}`, {
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: `${index + 1}级目录为空`,
+              },
+            ],
+          })(<Input placeholder="请输入" />)}
+          {keys.length !== 1 && index + 1 === keys.length ? (
+            <Icon
+              className="dynamic-delete-button"
+              type="minus-circle-o"
+              onClick={() => this.remove(k)}
+            />
+          ) : null}
+        </Form.Item>
+      </Col>
+    ));
+
+    return (
+      <Form className={styles['library-distribute-form']} onSubmit={this.handleSubmit}>
+        <Row gutter={16}>
+          {formItems}
+          <Col span={6}>
+            <Form.Item label={'文件名'} hasFeedback>
+              {getFieldDecorator('filename', {
+                validateTrigger: ['onChange', 'onBlur'],
+                rules: [
+                  {
+                    required: true,
+                    whitespace: true,
+                    validator: this.validateFilename,
+                  },
+                ],
+              })(<Input placeholder="请输入" />)}
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+}
+
+export default DistributeForm;
--- src/pages/MDEditor/index.tsx
+++ src/pages/MDEditor/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
+import { Modal, Row, Col, Form, Button, Icon } from 'antd';
 import { connect } from 'dva';
 import { Component } from '@/components/BaseComponent';
-import MarkdownEditor from '@/components/MarkdownEditor';
+import { markdownEditorProps } from '@/types/markdownEditor';
 
-@connect(({}) => ({}))
-class SubModule extends Component {
+import MarkdownEditor from '@/components/MarkdownEditor';
+import DistributeForm from './distributeForm';
+
+const DistributeFormModule = Form.create({ name: 'distribute_form' })(DistributeForm);
+
+@connect(({ markdownEditor }) => ({
+  contentText: markdownEditor.contentText,
+}))
+class MDEditorPage extends Component<markdownEditorProps, any> {
+  state = {
+    modalType: 'info',
+  };
+
   componentDidMount() {
     console.log('MarkdownEditor - componentDidMount !!!!');
   }
+
+  /**
+   * confirm click ok
+   * @param value      -  表单数据 data ( 分发路径数据 )
+   * @returns {Promise<any>}
+   */
+  modalHandleOK = value => {
+    return new Promise((resolve, reject) => {
+      const { dispatch, contentText } = this.props;
+      dispatch({
+        type: 'library/storeLibraryArticle',
+        payload: {
+          contentText: contentText,
+          pathObj: value,
+        },
+        callback: res => {
+          if (res) {
+            console.log('请求完成后返回的结果' + res);
+            resolve();
+          }
+        },
+      });
+    }).catch(() => console.log('Oops errors!'));
+  };
+
+  /**
+   * handle save - 将传递到子组件 DistributeFormModule 中的表单提交事件中执行
+   * @param e
+   * @param value  -  表单数据 data ( 分发路径数据 )
+   */
+  handleSave = (e, value): void => {
+    e.preventDefault();
+    Modal[this.state.modalType]({
+      title: '确定要提交吗',
+      content: '确定要提交吗',
+      onOk: () => this.modalHandleOK(value),
+      onCancel() {},
+    });
+  };
 
   render() {
     return (
@@ -16,10 +67,46 @@ class SubModule extends Component {
         <a href="https://www.jianshu.com/p/191d1e21f7ed" target="_blank">
           Markdown 书写规范
         </a>
-        <MarkdownEditor />
+        <Row>
+          <Col span={24}>
+            <Button onClick={() => this['child_form'].add()}>
+              <Icon type="plus-circle" />
+              Add
+            </Button>
+            <Button
+              style={{ marginLeft: 8 }}
+              type="danger"
+              onClick={() => this['child_form'].handleReset()}
+            >
+              重置路径
+            </Button>
+            <Button
+              style={{ marginLeft: 8 }}
+              type="danger"
+              onClick={() => this['child_editor'].handleChange('')}
+            >
+              清空编辑器
+            </Button>
+            <Button
+              style={{ marginLeft: 8 }}
+              type="primary"
+              onClick={() => this['child_form'].handleSubmit(event)}
+            >
+              submit
+            </Button>
+          </Col>
+        </Row>
+        <DistributeFormModule
+          wrappedComponentRef={form => (this['child_form'] = form)}
+          handleSave={this.handleSave}
+        />
+        <MarkdownEditor
+          onRef={ref => (this.child_editor = ref)}
+          handleSave={this.handleSave.bind(this)}
+        />
       </div>
     );
   }
 }
 
-export default SubModule;
+export default MDEditorPage;
