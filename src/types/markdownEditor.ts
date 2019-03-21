ce940b7f386d31356611e7da5bd2a493cc950738
xilanhuadechuntian <Lensgcx@163.com>
2019-03-21 14:17:26 +0800

    markdownEditor
    markdownEditor

---
+++ src/types/markdownEditor.ts
@@ -0,0 +1,10 @@
+export interface markdownEditorProps {
+  dispatch: any;
+  contentText: string;
+  handleSave: Function;
+  handleChange: Function;
+  onRef: any;
+  child_editor: any;
+  child_form: any;
+  isExists: boolean;
+}
