67813ebe7c750fe9a55e48d05f07fabcb289adc0
xilanhuadechuntian <Lensgcx@163.com>
2019-03-21 11:27:52 +0800

    services - library.service
    services - library.service

--- src/services/library.service.ts
+++ src/services/library.service.ts
@@ -1,5 +1,9 @@
 import { POST } from '@/utils/request';
 
-export function libraryServices(data) {
+export function libraryGetServices(data) {
   return POST('/libary/interview/article', data);
+}
+
+export function libraryStoreServices(data) {
+  return POST('/libary/interview/store', data);
 }
