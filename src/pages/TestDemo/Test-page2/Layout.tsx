1c24ef04a8fec8069c04fc2c1f55fa5346b74ad4
xilanhuadechuntian <Lensgcx@163.com>
2019-03-25 11:06:32 +0800

    Layout
    Layout

--- src/pages/TestDemo/Test-page2/Layout.tsx
+++ src/pages/TestDemo/Test-page2/Layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Component } from '@/components/BaseComponent';
-import { router_interviewMap } from '../../../../config/core/distribute';
 
 @connect(({}) => ({}))
 class TestPage2 extends Component {
   componentDidMount() {
     console.log('test-page2');
-    console.log(router_interviewMap);
   }
 
   render() {
