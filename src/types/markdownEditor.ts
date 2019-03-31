beb93d8beafc8dfed00bcee901dbd6ec0ff39afa
xilanhuadechuntian <Lensgcx@163.com>
2019-03-31 11:02:50 +0800

    Update markdownEditor.ts

--- src/types/markdownEditor.ts
+++ src/types/markdownEditor.ts
@@ -1,10 +1,13 @@
 export interface markdownEditorProps {
   dispatch: any;
-  contentText: string;
-  handleSave: Function;
-  handleChange: Function;
+  fileExt: Array<string>; //文件名称- 合法后缀
+  pathPrefix: string; //path prefix
+  contentInfo: object; //文章内容信息
+  contentText: string; //文章内容主体
+  handleSave: Function; //文章保存
+  handleChange: Function; //文章改变
   onRef: any;
   child_editor: any;
   child_form: any;
-  isExists: boolean;
+  isExists: boolean; //路径文章是否存在
 }
